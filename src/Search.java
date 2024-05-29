37ad3733f75f2c72d080d16f40e3611a150ddb9a
20fut <231612@astanait.edu.kz>
2024-05-29 10:53:36 +0500

    1

--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,16 @@
-public class Search {
-}
+import java.util.*;
+
+public class Search<Vertex> {
+    protected int count;
+    protected Set<Vertex> marked;
+    protected Map<Vertex, Vertex> edgeTo;
+    protected final Vertex source;
+
+    public Search(Vertex source) {
+        this.source = source;
+        marked = new HashSet<>();
+        edgeTo = new HashMap<>();
+    }
+
+
+    }
