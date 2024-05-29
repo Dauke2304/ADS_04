60762afc6820d463e87bc50b9588e1f79d995be1
20fut <231612@astanait.edu.kz>
2024-05-29 10:54:27 +0500

    1

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,18 @@
-public class WeightedGraph {
-}
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class WeightedGraph<Vertex> {
+    private boolean undirected;
+    private Map<Vertex,Map<Vertex,Double>> map = new HashMap<>();
+    public WeightedGraph() {
+        this(true);
+    }
+    public WeightedGraph(boolean undirected) {
+        this.undirected = undirected;
+    }
+
+    public void addVertex(Vertex v) {
+        map.put(v, new HashMap<>());
+    }
